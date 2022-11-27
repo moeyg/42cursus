6c52e306d5301238d71b55bd56c10d76319411c8
moeyg <moooeyg@gmail.com>
2022-11-27 18:36:06 +0900

    Add : header file - libft.h

---
+++ Libft/includes/libft.h
@@ -0,0 +1,14 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+// struct
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int	ft_isascii(int c);
+
+#endif
