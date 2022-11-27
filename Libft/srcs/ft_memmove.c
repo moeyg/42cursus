057e4b17576492ef8967d1d37a75a5c54e54fd0d
moeyg <moooeyg@gmail.com>
2022-11-27 21:25:28 +0900

    Chore : Change variable name (source_buffer -> src_buffer)

--- Libft/srcs/ft_memmove.c
+++ Libft/srcs/ft_memmove.c
@@ -3,21 +3,21 @@
 void *ft_memmove(void *dest, const void *src, size_t length)
 {
     unsigned char   *dest_buffer;
-    unsigned char   *source_buffer;
+    unsigned char   *src_buffer;
 
     dest_buffer = (unsigned char *)dest;
-    source_buffer = (unsigned char *)src;
+    src_buffer = (unsigned char *)src;
     if (dest == src || length == 0)
         return (dest);
     if (dest < src)
     {
         while (length != 0)
-            *dest_buffer++ = *source_buffer++;
+            *dest_buffer++ = *src_buffer++;
     }
     else
     {
         while (length != 0)
-            *(dest_buffer + length) = *(source_buffer + length);
+            *(dest_buffer + length) = *(src_buffer + length);
             length --;
     }
     return (dest);
