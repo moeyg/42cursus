06b484e6ddbea39962712ce04a5aa3ca4c317aad
Dogyeom Park <dogpark@student.42seoul.kr>
2022-11-28 19:19:36 +0900

    Chore : Move files to directory

--- Libft/ft_isalpha.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-int ft_isalpha(int ascii_num)
-{
-    if (('a' <= ascii_num && ascii_num <= 'z')
-        || ('A' <= ascii_num && ascii_num <= 'Z'))
-        return (1);
-    return (0);
-}
