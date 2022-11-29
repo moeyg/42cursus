4db6a4b0891e21ca9ecba9e7a9bed155e9e4929c
moeyg <moooeyg@gmail.com>
2022-11-30 01:08:26 +0900

    Add : ft_atoi.c

--- Libft/Libft/ft_atoi.c
+++ Libft/Libft/ft_atoi.c
@@ -7,22 +7,24 @@ int ft_atoi(const char *str)
     long long   result;
 
     index = 0;
-    sign = -1;
+    sign = 1;
     result = 0;
-    while (str[index] >= 9 && str[index] <= 13 || str[index] == ' ')
+    while ((str[index] >= 9 && str[index] <= 13) || str[index] == ' ')
         index ++;
     if (str[index] == '+' || str[index] == '-')
     {
         if (str[index] == '-')
-            sign *= -1;
+            sign = -1;
         index ++;
     }
     while (str[index] >= '0' && str[index] <= '9')
     {
-        result = result * 10 + str[index] - '0';
-        if (result > 2147483647)
-            return (0);
+        result = result * 10 + (str[index] - '0');
         index ++;
     }
-    return (result);
+    if (result > 2147483648 && sign < 0)
+        return (0);
+    if (result > 2147483647 && sign > 0)
+        return (0);
+    return (sign * result);
 }
