ae791e6479fd0e2116f84494848a68fa795d44ee
moeyg <moooeyg@gmail.com>
2022-11-30 13:01:33 +0900

    Add : ft_strjoin.c

---
+++ Libft/Libft/ft_strjoin.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char    *ft_strjoin(char const *prefix, char const *suffix)
+{
+    size_t  prefix_length;
+    size_t  suffix_length;
+    char    *result;
+
+    if (prefix == 0 || suffix == 0)
+        return (0);
+    prefix_length = ft_strlen(prefix);
+    suffix_length = ft_strlen(suffix);
+    if (!(result = (char *)malloc(prefix_length + suffix_length + 1)))
+        return (0);
+    ft_memmove(result, prefix, prefix_length);
+    ft_memmove(result + prefix_length, suffix, suffix_length);
+    result[prefix_length + suffix_length] = '\0';
+    return (result);
+}
