13936db174aa8761e1f22bc22470424c30d9d627
moeyg <moooeyg@gmail.com>
2022-11-30 14:19:16 +0900

    Add : ft_strtrim.c

---
+++ Libft/Libft/ft_strtrim.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+char	*ft_strtrim(char const *str, char const *delete)
+{
+    size_t  start;
+    size_t  end;
+    char    *result;
+
+    start = 0;
+    end = ft_strlen(str) - 1;
+    if (str != '\0' && delete != '\0')
+    {
+        while (str[start] != '\0' && ft_strchr(delete, str[start]))
+            start ++;
+        while (str[end] != '\0' && ft_strchr(delete, str[end]))
+            end --;
+        if (!(result = (char *)malloc(end - start + 1)))
+            return (0);
+        ft_strlcpy(result, &str[start], start - end + 1);
+    }
+    return (result);
+}
