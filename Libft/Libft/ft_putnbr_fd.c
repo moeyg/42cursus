2de4e1298bd4da81037eb79c95273c05f5cc9218
moeyg <moooeyg@gmail.com>
2022-12-02 23:49:49 +0900

    Add : ft_putnbr_fd.c

---
+++ Libft/Libft/ft_putnbr_fd.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void    ft_putnbr_fd(int integer, int file_descriptor)
+{
+    unsigned int    num;
+
+    num = integer;
+    if (integer < 0)
+    {
+        ft_putchar_fd('-', file_descriptor);
+        num = -integer;
+    }
+    if (num >= 10)
+        ft_putchar_fd(num / 10, file_descriptor);
+    ft_putchar_fd(num % 10 + '0', file_descriptor);
+}
