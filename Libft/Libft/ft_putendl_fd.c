4f294c3caa4616cfc70c60994bb339f82f21fdbd
moeyg <moooeyg@gmail.com>
2022-12-02 23:50:18 +0900

    Add : ft_putendl_fd.c

---
+++ Libft/Libft/ft_putendl_fd.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void    ft_putendl_fd(char *str, int file_descriptor)
+{
+    size_t  index;
+
+    index = 0;
+    if (str[index] == '\0')
+        return ;
+    while (str[index] != '\0')
+    {
+        write(file_descriptor, str[index], 1);
+        index ++;
+    }
+    write(file_descriptor, "\n", 1);
+}
