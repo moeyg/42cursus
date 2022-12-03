5ec7e8e326efad2ff71ccd0edb2db3b34ec341f0
moeyg <moooeyg@gmail.com>
2022-12-03 14:20:00 +0900

    Chore : Change logic

--- Libft/Libft/ft_putstr_fd.c
+++ Libft/Libft/ft_putstr_fd.c
@@ -2,14 +2,5 @@
 
 void    ft_putstr_fd(char *str, int file_descriptor)
 {
-    size_t  index;
-
-    index = 0;
-    if (str[index] == '\0')
-        return ;
-    while (str[index] != '\0')
-    {
-        write(file_descriptor, str[index], 1);
-        index ++;
-    }
+    write(file_descriptor, str, ft_strlen(str));
 }
