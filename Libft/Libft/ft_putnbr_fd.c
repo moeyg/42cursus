1a44fc6ae200bc1aad9b92a5725f08b7e30d1328
moeyg <moooeyg@gmail.com>
2022-12-04 12:13:33 +0900

    Chore : Line 14, ft_putchar_fd -> ft_putnbr_fd

--- Libft/Libft/ft_putnbr_fd.c
+++ Libft/Libft/ft_putnbr_fd.c
@@ -11,6 +11,6 @@ void    ft_putnbr_fd(int integer, int file_descriptor)
         num = -integer;
     }
     if (num >= 10)
-        ft_putchar_fd(num / 10, file_descriptor);
+        ft_putnbr_fd(num / 10, file_descriptor);
     ft_putchar_fd(num % 10 + '0', file_descriptor);
 }
