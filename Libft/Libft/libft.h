279f2a1abc48d60b5aedc9daba98db090a4024a9
moeyg <moooeyg@gmail.com>
2023-01-07 00:32:12 +0900

    Chore : Edit line 58, add ;

--- Libft/Libft/libft.h
+++ Libft/Libft/libft.h
@@ -55,7 +55,7 @@ void    ft_lstadd_front(t_list **list, t_list *new);
 void    ft_lstadd_back(t_list **list, t_list *new);
 void    ft_lstdelone(t_list *lst, void (*del)(void *));
 void    ft_lstclear(t_list **list, void (*del)(void *));
-void    ft_lstiter(t_list *list, void (*func)(void *))
+void    ft_lstiter(t_list *list, void (*func)(void *));
 t_list  *ft_lstmap(t_list *list, void *(*func)(void *), void (*del)(void *));
 
 #endif
