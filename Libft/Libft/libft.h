66217add96f90818f1264293cdcb0c2a92cfd6fb
moeyg <moooeyg@gmail.com>
2023-01-07 00:22:25 +0900

    Chore : Forward declaration ft_lstadd_front

--- Libft/Libft/libft.h
+++ Libft/Libft/libft.h
@@ -51,5 +51,6 @@ void    ft_putnbr_fd(int integer, int file_descriptor);
 int 	ft_lstsize(t_list *list);
 t_list  *ft_lstnew(void *content);
 t_list  *ft_lstlast(t_list *list);
+void    ft_lstadd_front(t_list **list, t_list *new);
 
 #endif
