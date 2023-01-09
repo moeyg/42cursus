2e8e75e417998d70907e3aa79bfcd4177d64c8dd
moeyg <moooeyg@gmail.com>
2023-01-09 19:00:57 +0900

    Chore : Add 42 header tag

--- Libft/Libft/ft_strdup.c
+++ Libft/Libft/ft_strdup.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moeyg <moeyg@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 12:55:07 by dogpark           #+#    #+#             */
+/*   Updated: 2023/01/09 19:00:43 by moeyg            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_strdup(const char *src)
+char	*ft_strdup(const char *src)
 {
-    size_t  index;
-    size_t  length;
-    char    *dest;
+	size_t	index;
+	size_t	length;
+	char	*dest;
 
-    length = ft_strlen(src);
-    dest = (char *)malloc(sizeof(char) * (length + 1));
-    if (!(dest))
-        return (0);
-    index = 0;
-    while (index < length)
-    {
-        dest[index] = src[index];
-        index ++;
-    }
-    dest[index] = '\0';
-    return (dest);
+	length = ft_strlen(src);
+	dest = (char *)malloc(sizeof(char) * (length + 1));
+	if (dest == NULL)
+		return (0);
+	index = 0;
+	while (index < length)
+	{
+		dest[index] = src[index];
+		index ++;
+	}
+	dest[index] = '\0';
+	return (dest);
 }
