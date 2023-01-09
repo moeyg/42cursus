a94a4bbd647a6ffd1e9073a2de318f178d41de94
moeyg <moooeyg@gmail.com>
2023-01-09 18:50:38 +0900

    Chore : Add overflow prevention code

--- Libft/Libft/ft_calloc.c
+++ Libft/Libft/ft_calloc.c
@@ -1,12 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moeyg <moeyg@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 12:49:29 by dogpark           #+#    #+#             */
+/*   Updated: 2023/01/09 18:26:02 by moeyg            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void    *ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-    void    *result;
+	void	*result;
 
-    result = malloc(size * count);
-    if (!(result))
-        return (0);
-    ft_bzero(result, size * count);
-    return (result);
+	if (count * size > 4294967295)
+		return (0);
+	result = malloc(size * count);
+	if (result == NULL)
+		return (0);
+	ft_bzero(result, size * count);
+	return (result);
 }
