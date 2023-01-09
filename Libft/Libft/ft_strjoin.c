9018de9daf7b5f2231cd97fcf4f5df5db9ec7d81
moeyg <moooeyg@gmail.com>
2023-01-09 19:26:47 +0900

    Chore : Add 42 header tag

--- Libft/Libft/ft_strjoin.c
+++ Libft/Libft/ft_strjoin.c
@@ -1,20 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moeyg <moeyg@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 12:55:17 by dogpark           #+#    #+#             */
+/*   Updated: 2023/01/09 19:01:35 by moeyg            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_strjoin(char const *prefix, char const *suffix)
+char	*ft_strjoin(char const *prefix, char const *suffix)
 {
-    size_t  prefix_length;
-    size_t  suffix_length;
-    char    *result;
+	size_t	prefix_length;
+	size_t	suffix_length;
+	char	*result;
 
-    if (prefix == 0 || suffix == 0)
-        return (0);
-    prefix_length = ft_strlen(prefix);
-    suffix_length = ft_strlen(suffix);
-    result = (char *)malloc(prefix_length + suffix_length + 1);
-    if (!(result))
-        return (0);
-    ft_strlcpy(result, prefix, prefix_length);
-    ft_strlcat(result + prefix_length, suffix, suffix_length);
-    result[prefix_length + suffix_length] = '\0';
-    return (result);
+	if (prefix == NULL || suffix == NULL)
+		return (0);
+	prefix_length = ft_strlen(prefix);
+	suffix_length = ft_strlen(suffix);
+	result = (char *)malloc(prefix_length + suffix_length + 1);
+	if (result == NULL)
+		return (0);
+	ft_strlcpy(result, prefix, prefix_length + 1);
+	ft_strlcat(result, suffix, prefix_length + suffix_length + 1);
+	return (result);
 }
--- Libft/Libft/ft_strlcat.c
+++ Libft/Libft/ft_strlcat.c
@@ -1,23 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 15:24:12 by dogpark           #+#    #+#             */
+/*   Updated: 2023/01/08 20:37:01 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t dest_size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-    size_t  index;
-    size_t  dest_length;
-    size_t  src_length;
+	size_t	index;
+	size_t	dest_length;
+	size_t	src_length;
 
-    index = 0;
-    dest_length = ft_strlen(dest);
-    src_length = ft_strlen(src);
-    if (dest == 0 || src == 0)
-        return (0);
-    if (dest_length >= dest_size - 1)
-        return (dest_length + src_length);
-    while (src[index] != '\0' && index < dest_size - dest_length - 1)
-    {
-        dest[dest_length + index] = src[index];
-        index ++;
-    }
-    dest[dest_length + index] = '\0';
-    return (dest_length + src_length);
+	dest_length = ft_strlen(dest);
+	src_length = ft_strlen(src);
+	index = 0;
+	if (size <= dest_length)
+		return (src_length + size);
+	while (index < size - dest_length - 1 && src[index])
+	{
+		dest[dest_length + index] = src[index];
+		index++;
+	}
+	dest[dest_length + index] = '\0';
+	return (dest_length + src_length);
 }
