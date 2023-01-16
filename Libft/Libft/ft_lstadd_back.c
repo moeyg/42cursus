9a5503954d787490254d8fcbbcb24d2b37e88567
Dogyeom Park <dogpark@student.42seoul.kr>
2023-01-16 17:33:24 +0900

    Chore : Edit 42 header

--- Libft/Libft/ft_lstadd_back.c
+++ Libft/Libft/ft_lstadd_back.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moeyg <moeyg@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 12:50:16 by dogpark           #+#    #+#             */
 /*   Updated: 2023/01/16 16:33:46 by moeyg            ###   ########.fr       */
