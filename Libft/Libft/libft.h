6762ae73408c8510edd59d92b309ad0ecb9a31b9
moeyg <moooeyg@gmail.com>
2023-01-16 17:28:36 +0900

    Chore : Function sort theorem

--- Libft/Libft/libft.h
+++ Libft/Libft/libft.h
@@ -3,25 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dogpark <dogpark@student.42.fr>            +#+  +:+       +#+        */
+/*   By: moeyg <moeyg@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 12:56:45 by dogpark           #+#    #+#             */
-/*   Updated: 2023/01/08 16:30:06 by dogpark          ###   ########.fr       */
+/*   Updated: 2023/01/16 17:14:05 by moeyg            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+/* Standard header file */
 # include <unistd.h>
 # include <stdlib.h>
 
+/* List construct */
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
 
+/* Character management function */
 int		ft_isalpha(int ascii_num);
 int		ft_isdigit(int ascii_num);
 int		ft_isalnum(int ascii_num);
@@ -30,6 +33,7 @@ int		ft_isprint(int ascii_num);
 int		ft_toupper(int ascii_num);
 int		ft_tolower(int ascii_num);
 
+/* String function */
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size);
 size_t	ft_strlcat(char *dest, const char *src, size_t dest_size);
@@ -44,6 +48,12 @@ void	ft_striteri(char *str, void (*func)(unsigned int, char*));
 int		ft_atoi(const char *str);
 char	*ft_itoa(int integer);
 
+char	*ft_substr(char const *str, unsigned int start, size_t length);
+char	*ft_strjoin(char const *prefix, char const *suffix);
+char	*ft_strtrim(char const *str, char const *delete);
+char	**ft_split(char const *str, char delimiter);
+
+/* Memory function */
 void	*ft_memset(void *ptr, int value, size_t length);
 void	*ft_memcpy(void *dest, const void *src, size_t length);
 void	*ft_memmove(void *dest, const void *src, size_t length);
@@ -53,16 +63,13 @@ int		ft_memcmp(const void *str1, const void *str2, size_t length);
 void	ft_bzero(void *str, size_t size);
 void	*ft_calloc(size_t count, size_t size);
 
-char	*ft_substr(char const *str, unsigned int start, size_t length);
-char	*ft_strjoin(char const *prefix, char const *suffix);
-char	*ft_strtrim(char const *str, char const *delete);
-char	**ft_split(char const *str, char delimiter);
-
+/* File descriptor function */
 void	ft_putchar_fd(char character, int file_descriptor);
 void	ft_putstr_fd(char *str, int file_descriptor);
 void	ft_putendl_fd(char *str, int file_descriptor);
 void	ft_putnbr_fd(int integer, int file_descriptor);
 
+/* List function */
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *list);
 int		ft_lstsize(t_list *list);
