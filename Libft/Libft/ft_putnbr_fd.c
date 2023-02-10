3f2ff3eaa57a9f5288dfd5c4602fec64859035eb
Dogyeom Park <dogpark@student.42seoul.kr>
2023-02-10 17:29:56 +0900

    Chore : Edit code scope

--- Libft/Libft/ft_putnbr_fd.c
+++ Libft/Libft/ft_putnbr_fd.c
@@ -23,6 +23,8 @@ void	ft_putnbr_fd(int integer, int file_descriptor)
 		num = -integer;
 	}
 	if (num >= 10)
+	{
 		ft_putnbr_fd(num / 10, file_descriptor);
+	}
 	ft_putchar_fd(num % 10 + '0', file_descriptor);
 }
