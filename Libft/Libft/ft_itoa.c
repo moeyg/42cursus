d2eae35fad5d04ec48bf89c47fe50859a2e97456
Dogyeom Park <dogpark@student.42seoul.kr>
2023-02-10 17:23:53 +0900

    Chore : Edit code scope

--- Libft/Libft/ft_itoa.c
+++ Libft/Libft/ft_itoa.c
@@ -24,16 +24,24 @@ char	*ft_itoa(int integer)
 	back = get_integer_length(integer);
 	result = (char *)malloc(back + 1);
 	if (result == NULL)
+	{
 		return (0);
+	}
 	if (integer < 0)
+	{
 		result[front++] = '-';
+	}
 	result[back--] = 0;
 	while (front <= back)
 	{
 		if (integer < 0)
+		{
 			result[back--] = -(integer % 10) + '0';
+		}
 		else
+		{
 			result[back--] = integer % 10 + '0';
+		}
 		integer /= 10;
 	}
 	return (result);
@@ -44,9 +52,13 @@ static int	get_integer_length(int integer)
 	int	count;
 
 	if (integer < 0)
+	{
 		count = 2;
+	}
 	else
+	{
 		count = 1;
+	}
 	while (integer / 10 != 0)
 	{
 		count ++;
