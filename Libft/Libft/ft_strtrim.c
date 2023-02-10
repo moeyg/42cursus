7bddae901a42403378ee45cf3a4be2b4f91cb81f
Dogyeom Park <dogpark@student.42seoul.kr>
2023-02-10 17:34:30 +0900

    Chore : Edit code scope

--- Libft/Libft/ft_strtrim.c
+++ Libft/Libft/ft_strtrim.c
@@ -19,17 +19,27 @@ char	*ft_strtrim(char const *str, char const *delete)
 	size_t	back;
 
 	if (str == NULL || delete == NULL)
+	{
 		return (0);
+	}
 	front = 0;
 	back = ft_strlen(str);
 	while (str[front] && ft_strchr(delete, str[front]))
+	{
 		front ++;
+	}
 	while (str[back - 1] && ft_strchr(delete, str[back - 1]) && back > front)
+	{
 		back --;
+	}
 	result = (char *)malloc(sizeof(char) * (back - front + 1));
 	if (result == NULL)
+	{
 		return (0);
+	}
 	else
+	{
 		ft_strlcpy(result, &str[front], back - front + 1);
+	}
 	return (result);
 }
