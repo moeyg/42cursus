698b97bac245e9ff7fe4e13e76c372252f525613
Dogyeom Park <dogpark@student.42seoul.kr>
2023-03-05 22:17:13 +0900

    Chore : Change variable name

--- Libft/Libft/ft_lstiter.c
+++ Libft/Libft/ft_lstiter.c
@@ -12,15 +12,15 @@
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *list, void (*func)(void *))
+void	ft_lstiter(t_list *node, void (*func)(void *))
 {
-	if (list == NULL || func == NULL)
+	if (node == NULL || func == NULL)
 	{
 		return ;
 	}
-	while (list != NULL)
+	while (node != NULL)
 	{
-		func(list->content);
-		list = list->next;
+		func(node->content);
+		node = node->next;
 	}
 }
