3d582a0284f485df2b5a531a2ce48a1aa0c02e43
Dogyeom Park <dogpark@student.42seoul.kr>
2023-03-29 20:12:24 +0900

    Chore : Add 42 header & Check norminette

--- Circle 1/get_next_line/get_next_line/get_next_line_bonus.h
+++ Circle 1/get_next_line/get_next_line/get_next_line_bonus.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 14:58:42 by dogpark           #+#    #+#             */
+/*   Updated: 2023/03/16 15:01:40 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
@@ -9,11 +21,11 @@
 #  define OPEN_MAX 10240
 # endif
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-size_t  ft_strlen(const char *str);
-size_t  ft_strlcat(char *dest, const char *src, size_t dest_size);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcat(char *dest, const char *src, size_t dest_size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size);
 char	*ft_strdup(const char *src);
 char	*ft_strjoin(char *prefix, char *suffix);
