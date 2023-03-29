dd8b4ed922efc4be63b6282079eb04c007e1e3f4
Dogyeom Park <dogpark@student.42seoul.kr>
2023-03-29 20:06:19 +0900

    Chore : Add 42 header

--- Circle 1/ft_printf/ft_printf/includes/ft_printf.h
+++ Circle 1/ft_printf/ft_printf/includes/ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogpark <dogpark@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/29 19:37:10 by dogpark           #+#    #+#             */
+/*   Updated: 2023/03/29 19:40:28 by dogpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -7,12 +19,12 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int     ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
 
-void    print_char(char character, int *count);
-void    print_string(char *string, int *count);
-void    print_int(int integer, int *count);
-void    print_unsigned_int(unsigned int integer, int *count);
-void    print_hexadecimal(char specifier, unsigned long hexadecimal, int *count);
+void	print_char(char character, int *count);
+void	print_string(char *string, int *count);
+void	print_int(int integer, int *count);
+void	print_unsigned_int(unsigned int integer, int *count);
+void	print_hexadecimal(char specifier, unsigned long hex, int *count);
 
 #endif
