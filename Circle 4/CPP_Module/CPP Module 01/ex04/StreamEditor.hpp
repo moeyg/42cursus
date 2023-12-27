7aaa7f65845344f8259263c4746a2aa20cb47a48
Dogyeom Park <moooeyg@gmail.com>
2023-12-27 19:39:34 +0900

    chore: The function name has been changed. (openFiles → openFile)

--- Circle 4/CPP_Module/CPP Module 01/ex04/StreamEditor.hpp
+++ Circle 4/CPP_Module/CPP Module 01/ex04/StreamEditor.hpp
@@ -16,7 +16,7 @@ private:
     std::string s1;
     std::string s2;
 
-    bool    openFiles(std::ifstream& inputFileStream, std::ofstream& outputFileStream);
+    bool    openFile(std::ifstream& inputFileStream, std::ofstream& outputFileStream);
 
 public:
     StreamEditor(const std::string& filename, const std::string& s1, const std::string& s2);
